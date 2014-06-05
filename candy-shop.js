c00766b7fe236cac506a193e4f44efbe0806297a
Justin Worsdale <whayler1@gmail.com>
2014-06-04 21:59:05 -0400

    docs

--- candy-shop.js
+++ candy-shop.js
@@ -1,146 +1,146 @@
 var data = {
-		title: 'Welcome to the Candy Shop',
-		
-		products: [
-			{
-				title: 'lollipop',
-				price: 0.5
-			},
-			{
-				title: 'teakettle',
-				price: 14.95
-			},
-			{
-				title: 'magic stick',
-				price: 89.12546
-			},
-			{
-				title: 'low rider',
-				price: 22450
-			},
-			{
-				title: 'champagne',
-				price: 45
-			}
-		],
-		
-		bag: [],
-		
-		subtotal: 0,
-		
-		tax: 0,
-		
-		total: 0
-	},
-	
-	controller = {
-		onAtbClick: function(e, model) {
-			
-			var product = model.data.products[model.index],
-				bag = model.data.bag,
-				i = 0,
-				updatePrice = model.controller.updatePrice;
-			
-			for(; i < bag.length; i++) {
-				
-				if(bag[i].title === product.title) {
-					
-					bag[i].quantity++;
-					updatePrice(model.data);
-					return;
-				}
-			}
-			
-			bag.push(product);
-			bag[bag.length - 1].quantity = 1;
-			//console.log(product, bag);
-			updatePrice(model.data);
-		},
-		
-		addItem: function(e, model) {
-			
-			model.data.bag[model.index].quantity++;
-			model.controller.updatePrice(model.data);
-		},
-		
-		removeItem: function(e, model) {
-			
-			var index = model.index,
-				bag = model.data.bag,
-				product = bag[index],
-				updatePrice = model.controller.updatePrice;
-			
-			if(product.quantity > 1) {
-				
-				product.quantity--;
-				updatePrice(model.data);
-				return;
-			}
-			
-			bag.splice(index, 1);
-			updatePrice(model.data);
-		},
-		
-		updatePrice: function(data) {
-			
-			var bag = data.bag,
-				product,
-				subtotal = 0,
-				i = 0;
-			
-			for(; i < bag.length; i++) {
-				
-				product = bag[i];
-				
-				subtotal += product.price * product.quantity;
-			}
-			
-			data.subtotal = subtotal;
-			data.tax = subtotal * 0.08875;
-			data.total = subtotal + data.tax;
-		}
-	};
+    title: 'Welcome to the Candy Shop',
+    
+    products: [
+      {
+        title: 'lollipop',
+        price: 0.5
+      },
+      {
+        title: 'teakettle',
+        price: 14.95
+      },
+      {
+        title: 'magic stick',
+        price: 89.12546
+      },
+      {
+        title: 'low rider',
+        price: 22450
+      },
+      {
+        title: 'champagne',
+        price: 45
+      }
+    ],
+    
+    bag: [],
+    
+    subtotal: 0,
+    
+    tax: 0,
+    
+    total: 0
+  },
+  
+  controller = {
+    onAtbClick: function(e, model) {
+      
+      var product = model.data.products[model.index],
+        bag = model.data.bag,
+        i = 0,
+        updatePrice = model.controller.updatePrice;
+      
+      for(; i < bag.length; i++) {
+        
+        if(bag[i].title === product.title) {
+          
+          bag[i].quantity++;
+          updatePrice(model.data);
+          return;
+        }
+      }
+      
+      bag.push(product);
+      bag[bag.length - 1].quantity = 1;
+      //console.log(product, bag);
+      updatePrice(model.data);
+    },
+    
+    addItem: function(e, model) {
+      
+      model.data.bag[model.index].quantity++;
+      model.controller.updatePrice(model.data);
+    },
+    
+    removeItem: function(e, model) {
+      
+      var index = model.index,
+        bag = model.data.bag,
+        product = bag[index],
+        updatePrice = model.controller.updatePrice;
+      
+      if(product.quantity > 1) {
+        
+        product.quantity--;
+        updatePrice(model.data);
+        return;
+      }
+      
+      bag.splice(index, 1);
+      updatePrice(model.data);
+    },
+    
+    updatePrice: function(data) {
+      
+      var bag = data.bag,
+        product,
+        subtotal = 0,
+        i = 0;
+      
+      for(; i < bag.length; i++) {
+        
+        product = bag[i];
+        
+        subtotal += product.price * product.quantity;
+      }
+      
+      data.subtotal = subtotal;
+      data.tax = subtotal * 0.08875;
+      data.total = subtotal + data.tax;
+    }
+  };
 
 rivets.formatters.price = function(val) {
-	
-	var spl = String(val).split('.'),
-		dollarsArray = spl[0].split(''),
-		lastDollar = dollarsArray.length - 1,
-		pow,
-		i;
-	
-	if(dollarsArray.length > 3) {
-		
-		dollarsArray.reverse();
-		
-		for(i = lastDollar; i > -1; i--) {
-			
-			if(i % 3 === 0 && i !== 0) {
-				
-				dollarsArray.splice(i, 0, ',');
-			}
-		}
-		
-		spl[0] = dollarsArray.reverse().join('');
-	}
-	
-	if(spl.length > 1) {
-		
-		spl[1] = spl[1].substr(0, 2);
-	}else {
-		
-		spl[1] = '00';
-	}
-	
-	return '<abbr title="USD">$</abbr>' + spl.join('.');
+  
+  var spl = String(val).split('.'),
+    dollarsArray = spl[0].split(''),
+    lastDollar = dollarsArray.length - 1,
+    pow,
+    i;
+  
+  if(dollarsArray.length > 3) {
+    
+    dollarsArray.reverse();
+    
+    for(i = lastDollar; i > -1; i--) {
+      
+      if(i % 3 === 0 && i !== 0) {
+        
+        dollarsArray.splice(i, 0, ',');
+      }
+    }
+    
+    spl[0] = dollarsArray.reverse().join('');
+  }
+  
+  if(spl.length > 1) {
+    
+    spl[1] = spl[1].substr(0, 2);
+  }else {
+    
+    spl[1] = '00';
+  }
+  
+  return '<abbr title="USD">$</abbr>' + spl.join('.');
 };
 
 rivets.formatters.length = function(val) {
-	
-	return val.length;
+  
+  return val.length;
 }
 
 rivets.bind(document.querySelector('#candy-shop'), {
-	data: data,
-	controller: controller
+  data: data,
+  controller: controller
 });
--- readme.md
+++ readme.md
@@ -2,9 +2,13 @@
 
 A buddy recently turned me on to [rivets js](http://www.rivetsjs.com/). Besides being bundled with the epic [ember js](http://emberjs.com/) framework, it's a sleek little stand alone lib. Rivets is a [templating system](http://en.wikipedia.org/wiki/JavaScript_templating), which in and of itself is not so revolutionary, except unlike handlebars or mustache it has built in two-way binding. This means that, assuming everything is configured properly, when you update your data object the DOM will auto-magically update, and when you update properly associated DOM elements the data model will update. A one and done, one stop shop, configure your page and don't worry about your view and your data falling out of sync again.
 
+This tutorial is going to walk you through building a small mock-shopping app with a product array, a shopping bag and a bill calculator. This app will be for a candy shop.
+
 ### Hello Rivets
 
-The most basic thing you could do in rivets is use it's handlebars-like syntax to populate parts of the dom with data. This is called &ldquo;text content interpolation&rdquo;, but for our purposes it basically means defining where you want data to appear in the DOM by wrapping it in curly brackets: `{ somedata }`.
+The most basic thing you could do in rivets is use it's handlebars-like syntax to populate parts of the dom with data. This is called "text content interpolation", but for our purposes it basically means defining where you want data to appear in the DOM by wrapping it in curly brackets: `{ somedata }`.
+
+#### html
 
 Make a new html file and write the below markup in it.
 
@@ -18,10 +22,48 @@ Make a new html file and write the below markup in it.
       src="http://cdn.rawgit.com/mikeric/rivets/master/dist/rivets.min.js"></script>
   <body>
     <div id="candy-shop">
-      <h1 rv-text="data.title"></h1>
+      <h1>{ data.title }</h1>
     </div>
     
     <script type="text/javascript" src="candy-shop.js"></script>
   </body>
 </html>
+```
+
+In the head we pull in rivets script. Inside the body we are creating a div with an h1 tag. The h1 tag has `{ data.title }` in it. If you've used templating engines before this should look familiar to you.
+
+#### css
+
+We won't be editing this css at all. I'd just cut and paste it and name the file "candy-shop.css". This will just help the app fit on your screen a bit nicer, but is not in any way necessary to this tutorial.
+
+```css
+ul {
+  margin-left: 0;
+  padding-left: 0;
+}
+
+li {
+  display: inline-block;
+  padding: 0 20px;
+  border-left: 1px dotted grey;
+}
+
+li:first-child {
+  padding-left: 0;
+  border-left: none;
+}
+```
+
+#### js
+
+This is where things will start to get fun. Name this file "candy-shop.js".
+
+```js
+var data = {
+    title: 'Welcome to the Candy Shop'
+    };
+
+rivets.bind(document.querySelector('#candy-shop'), {
+  data: data
+});
 ```
